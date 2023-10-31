f7ad9a1ccf78d241ffee21d30f3e4df67ae92dc7
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2023-10-31 18:28:26 -0300

    Configuração do WebSocket

---
+++ write-me/src/main/java/com/writeme/chat/WriteMeApplication.java
@@ -0,0 +1,13 @@
+package com.writeme.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteMeApplication.class, args);
+	}
+
+}
---
+++ write-me/src/test/java/com/writeme/chat/WriteMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writeme.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
